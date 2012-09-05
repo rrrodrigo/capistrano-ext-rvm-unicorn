6c6a8901268bec3cb3177e1125cce11a2aebb884
Marcin Bajer <bajer@tigana.pl>
2012-09-05 22:41:00 +0200

    starting unicorn with 'bundle exec'

--- lib/capistrano/ext/rvm-unicorn.rb
+++ lib/capistrano/ext/rvm-unicorn.rb
@@ -5,7 +5,7 @@ Capistrano::Configuration.instance.load do
   namespace :deploy do
     desc "Start unicorn server"
     task :start, :roles => :app, :except => { :no_release => true } do
-      run "cd #{current_path} && rvm '#{rvm_ruby_string}' && unicorn_rails -c #{unicorn_config} -E #{rails_env} -D"
+      run "cd #{current_path} && rvm '#{rvm_ruby_string}' && bundle exec unicorn_rails -c #{unicorn_config} -E #{rails_env} -D"
     end
 
     desc "Stop unicorn server"
